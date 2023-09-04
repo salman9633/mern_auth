15251a39c97f48f32f5118f2201d2973ed403098
salmanfaris <salmanvaipees913@gmail.com>
2023-09-04 18:08:14 +0530

    user signup backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import User from '../models/userModel.js'
+import {User} from '../models/userModel.js'
 
 const auth = asyncHandler(async (req, res) => {
     res.status(401)
@@ -10,29 +10,39 @@ const auth = asyncHandler(async (req, res) => {
 //Desc     Register New User
 //Route    POST api/users
 //Access   public
-const registerUser =asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     console.log(req.body);
-    const {name,email,password}=req.body
-    const userExist= await User.findOne({email}) 
-    if(userExist) {
+    const { name, email, password } = req.body
+    const userExist = await User.findOne({ email })
+    if (userExist) {
         res.status(400);
         throw new Error('user already exist')
     }
 
-    const user= await User.create({
+    const user = await User.create({
         name,
         email,
         password
     })
-    res.status(200).json({
-        message: "register user"
-    });
+
+    if (user) {
+        res.status(201)
+            .json({
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            })
+    }else{
+        res.status(400);
+        throw new Error('inavalid data')
+    }
+    
 })
 
 //Desc     Logout
 //Route    POST api/users/logout
 //Access   public
-const logoutUser =asyncHandler(async (req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "Logout user"
     });
@@ -41,7 +51,7 @@ const logoutUser =asyncHandler(async (req, res) => {
 //Desc     Get user Prodfile
 //Route    GET api/users/profile
 //Access   Private
-const getUserProfile =asyncHandler(async (req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "user profile"
     });
@@ -50,7 +60,7 @@ const getUserProfile =asyncHandler(async (req, res) => {
 //Desc     Update user Prodfile
 //Route    PUT api/users/profile
 //Access   Private
-const updaterUserProfile =asyncHandler(async (req, res) => {
+const updaterUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "update profile"
     });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -22,9 +22,18 @@ const userSchema = mongoose.Schema({
     }
 );
 
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified('password')) next();
+
+    const salt = await bcrypt.genSalt(9);
+    this.password = await bcrypt.hash(this.password, salt)
+
+})
+
 const User = mongoose.model('User', userSchema);
 
-export default { User }
+export {User}  
 
 
 
